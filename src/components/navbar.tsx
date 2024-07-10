7777c9ee67a34a5af4fa6df18e86b2439e452ddd
Pancham <{panchamb63@gmail.com}+{Pancham555}@users.noreply.github.com>
2024-07-10 15:40:34 +0530

    Improved the sidebar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,6 +18,12 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
@@ -58,14 +64,17 @@ const Navbar = () => {
               </Button>
             </SheetTrigger>
             <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Are you absolutely sure?</SheetTitle>
-                <SheetDescription>
-                  This action cannot be undone. This will permanently delete
-                  your account and remove your data from our servers.
-                </SheetDescription>
+              <SheetHeader className="mt-5">
+                <SheetTitle className="flex gap-5">
+                  <Logo />
+                </SheetTitle>
               </SheetHeader>
-              <ThemeButton />
+              <SheetDescription>
+                <SmallAboutFry />
+                <SmallFeatures />
+                <SmallResources />
+                <SmallCompany />
+              </SheetDescription>
             </SheetContent>
           </Sheet>
         </div>
@@ -249,16 +258,132 @@ const More = () => {
   );
 };
 
+const SmallAboutFry = () => {
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>About UIFRY?</AccordionTrigger>
+        <AccordionContent>
+          <div className="grid grid-cols-1">
+            {["", "", "", "", "", ""].map((_, i) => {
+              return (
+                <div
+                  key={i}
+                  className={cn(
+                    "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                  )}
+                >
+                  <div className="text-sm font-medium leading-none">Title</div>
+                  <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit...
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+const SmallFeatures = () => {
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Features</AccordionTrigger>
+        <AccordionContent>
+          <div className="grid grid-cols-1">
+            {["", "", "", "", "", ""].map((_, i) => {
+              return (
+                <div
+                  key={i}
+                  className={cn(
+                    "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                  )}
+                >
+                  <div className="text-sm font-medium leading-none">Title</div>
+                  <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit...
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+const SmallResources = () => {
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Resources</AccordionTrigger>
+        <AccordionContent>
+          <div className="grid grid-cols-1">
+            {["", "", "", "", "", ""].map((_, i) => {
+              return (
+                <div
+                  key={i}
+                  className={cn(
+                    "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                  )}
+                >
+                  <div className="text-sm font-medium leading-none">Title</div>
+                  <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit...
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+const SmallCompany = () => {
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Company</AccordionTrigger>
+        <AccordionContent>
+          <div className="grid grid-cols-1">
+            {["", "", "", "", "", ""].map((_, i) => {
+              return (
+                <div
+                  key={i}
+                  className={cn(
+                    "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                  )}
+                >
+                  <div className="text-sm font-medium leading-none">Title</div>
+                  <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit...
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
 const ThemeButton = () => {
   const { setTheme } = useTheme();
   return (
     <Button variant="outline" size="icon">
       <Sun
-        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 duration-200"
         onClick={() => setTheme("dark")}
       />
       <Moon
-        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 duration-200"
         onClick={() => setTheme("light")}
       />
       <span className="sr-only">Toggle theme</span>
